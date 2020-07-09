01849d5f2b4917fcb6932e10320baf8c9a9fbf39
Alex Magana <46731475+jamcoding@users.noreply.github.com>
2020-07-08 19:49:32 -0500

    finished 00-05 exercises

--- src/02_datatypes.py
+++ src/02_datatypes.py
@@ -11,11 +11,21 @@ on a string and an integer.
 x = 5
 y = "7"
 
+# convert integer to string
+x_str = str(x)
+
+# convert string to integer
+y_int = int(y)
+
 # Write a print statement that combines x + y into the integer value 12
 
 # YOUR CODE HERE
+print(x + int(y))
+print(x + y_int)
 
 
 # Write a print statement that combines x + y into the string value 57
 
 # YOUR CODE HERE
+print(f'{x}{y}')
+print(x_str + y)
--- src/03_modules.py
+++ src/03_modules.py
@@ -10,22 +10,27 @@ import sys
 
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
+print("This is the name of the script: ", sys.argv[0])
 
 # Print out the OS platform you're using:
 # YOUR CODE HERE
+print("I am using OS platform: ", sys.platform)
 
 # Print out the version of Python you're using:
 # YOUR CODE HERE
-
+print("I am using Python version: ", sys.version)
 
 import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
 # Print the current process ID
 # YOUR CODE HERE
+print("My current process ID: ", os.getpid())
 
 # Print the current working directory (cwd):
 # YOUR CODE HERE
+print("My current working directory: ", os.getcwd())
 
 # Print out your machine's login name
 # YOUR CODE HERE
+print("My machine's login name: ", os.getlogin())
